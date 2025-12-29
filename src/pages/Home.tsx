5055a7593397fc0ef2f54de2cd3fdef5853b9690
bertoramos <ramossanchezalberto@gmail.com>
2025-12-29 15:09:19 +0000

    feat: add Home page content (demo image)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import { useEffect, useState } from 'react';
 import './Home.css';
 import { loadCV } from '../controller/cv_load';
@@ -9,18 +9,39 @@ const Home: React.FC<{ cv: CVType | null }> = ({ cv }) => {
 
   return (
     <>
-      {cv && <p>{cv.about.about_description}</p>}
-      {cv && <p>{cv.about.about_description}</p>}
-      {cv && <p>{cv.about.about_description}</p>}
-      {cv && <p>{cv.about.about_description}</p>}
-      {cv && <p>{cv.about.about_description}</p>}
-      {cv && <p>{cv.about.about_description}</p>}
-      {cv && <p>{cv.about.about_description}</p>}
-      {cv && <p>{cv.about.about_description}</p>}
-      {cv && <p>{cv.about.about_description}</p>}
-      {cv && <p>{cv.about.about_description}</p>}
-      {cv && <p>{cv.about.about_description}</p>}
-      {cv && <p>{cv.about.about_description}</p>}
+      <IonGrid id="home-grid">
+        <IonRow style={{ height: '100%' }}>
+          <IonCol
+            id="home-image-column"
+            size="12"
+            size-md="6"
+            push-md="6"
+          >
+            <img
+              id="home-image"
+              src="/portfolio-page/assets/images/home/profile_home.jpg"
+              alt="Home photo"
+            />
+          </IonCol>
+          <IonCol
+            id="home-text-column"
+            size="12"
+            size-md="6"
+            pull-md="6"
+          >
+            <div id="home-text">
+              <p id="home-title">
+                Hey there,<br />
+                Alberto here
+              </p>
+              <p id="home-subtitle">
+                I'm computer scientist from<br />
+                Canary Island, Spain
+              </p>
+            </div>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
     </>
   );
 };
